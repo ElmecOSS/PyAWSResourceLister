9d324bc2d6c95ec738ab31d3c152f8ae8375ad86
cominoli <luca.cominoli@elmec.it>
2022-08-11 13:18:37 +0200

    Folder

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='awsresourcelister',
-    version='1.0.0',
+    version='0.0.1',
     packages=setuptools.find_packages(),
     url='',
     author='Cominoli Luca, Dalle Fratte Andrea',
