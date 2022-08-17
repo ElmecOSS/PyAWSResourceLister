a1dc85be7066392ac65fe52b257430a1a567c32e
cominoli <luca.cominoli@elmec.it>
2022-08-17 09:49:43 +0200

    Release versione 1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='awsresourcelister',
-    version='0.0.1',
+    version='1.0.0',
     packages=setuptools.find_packages(),
     url='',
     author='Cominoli Luca, Dalle Fratte Andrea',
